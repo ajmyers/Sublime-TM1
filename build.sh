fd74280b26336f98fbc8456b4545724109ee3dbd
Andrew Myers <me@ajmyers.net>
2022-02-23 11:54:35 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 
 rm -rf "/Users/andrewmyers/Library/Application Support/Sublime Text 3/Installed Packages/TM1 Planning Analytics Developer Tools.sublime-package" || true
-zip -rv "/Users/andrewmyers/Library/Application Support/Sublime Text 3/Installed Packages/TM1 Planning Analytics Developer Tools.sublime-package" * -x "*.git*" -x "env/*"
+zip -rv "/Users/andrewmyers/Library/Application Support/Sublime Text 3/Installed Packages/TM1 Planning Analytics Developer Tools.sublime-package" . -x "*.git*" -x "env/*"
 
 rm -rf "/Users/andrewmyers/Library/Application Support/Sublime Text 3/Packages/TM1py" || true
-cp -Rv "/Users/andrewmyers/Documents/Projects/TM1py" "/Users/andrewmyers/Library/Application Support/Sublime Text 3/Packages/TM1py"
+#cp -Rv "/Users/andrewmyers/Documents/Projects/TM1py" "/Users/andrewmyers/Library/Application Support/Sublime Text 3/Packages/TM1py"
 
 killall sublime_text
 sleep .5
